7c668bd7cd5bc591bc9a566a469e3a6cdd722598
Adrián Fuentes <66267990+elektxo@users.noreply.github.com>
2022-09-22 13:03:16 +0100

    backup

--- PRO/ut0/capicua_entero.py
+++ PRO/ut0/capicua_entero.py
@@ -7,9 +7,7 @@ multiplicador = 1
 for i in range(len(n_aux)):
     n_invertido = n_invertido + (int(n_aux[i])*multiplicador)
     multiplicador = multiplicador*10
-
-print(n_invertido, n_original)
-
+    
 if int(n_original) == n_invertido:
     print("Es capicua")
 else:
